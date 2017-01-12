b0f11ff97c15ef7dccc965db3203d1aa9501d7cd
Tomotz <tom.mottes@gmail.com>
2017-01-12 17:08:29 +0200

    started git

---
+++ sendTestQuery.py
@@ -0,0 +1,12 @@
+import MySQLdb as mdb
+DB_QUERY = "SELECT * FROM writer"
+
+
+def connectToDB():
+	db = mdb.connect("127.0.0.1", "DbMysql17", "DbMysql17", "DbMysql17", port=19821)
+	with db:
+		cur = db.cursor()
+		cur.execute(DB_QUERY)
+
+if __name__ == "__main__":
+	connectToDB()
