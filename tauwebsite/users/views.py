6a035b7900591464ef59a996689da47d0e52f382
LiadW <liadwg2@gmail.com>
2017-01-17 15:05:58 +0200

    added login view for login and signup

--- tauwebsite/users/views.py
+++ tauwebsite/users/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseBadRequest
 from rest_framework.views import APIView
 from rest_framework import serializers, permissions
 
@@ -8,5 +8,17 @@ def index(request):
 class LoginView(APIView):
     permission_classes = (permissions.AllowAny,)
 
+    # get user by username
     def get(self, request, user_name):
+        return HttpResponseBadRequest()
+        return HttpResponseForbidden()
         return HttpResponse("Hello, world. UNAME - %s" % user_name)
+
+    # create new user
+    def post(self, request, *args, **kwargs):
+        first_name = request.data.get("first_name")
+        last_name = request.data.get("last_name")
+        address = request.data.get("address")
+
+        print "Got data for new user: first name '%s', last name '%s', address %s" % (first_name, last_name, address)
+        return HttpResponse("Hello, world. FNAME - %s" % first_name)
