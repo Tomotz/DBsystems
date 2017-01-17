fc92cca91d8ec4c1b7e6471b7c5cb3c4041b2156
LiadW <liadwg2@gmail.com>
2017-01-17 20:36:02 +0200

    basic login/signup form validation

--- app/controllers/LoginController.js
+++ app/controllers/LoginController.js
@@ -19,6 +19,11 @@ View1Ctrl.controller('LoginController', ['$scope', '$state', '$rootScope', 'Logi
 
     $scope.submit_login = function (username) {
         console.log(username);
+        $scope.user_name_err = false;
+        if (!username){
+            $scope.user_name_err = true;
+            return;
+        }
         LoginService.user_login(username).then(function(user) {
             if (user){
                 console.log(user);
@@ -31,22 +36,45 @@ View1Ctrl.controller('LoginController', ['$scope', '$state', '$rootScope', 'Logi
     };
 
     $scope.submit_signup = function () {
-        var address = autocomplete.getPlace();
-        address.lat = address.geometry.location.lat();
-        address.lng = address.geometry.location.lng();
-        var user_details = {
-            "user_name"  : $scope.username,
-            "first_name" : $scope.first_name,
-            "last_name"  : $scope.last_name,
-            "address"    : address
-        };
-        console.log("HOLA! ", user_details);
-        LoginService.user_signup(user_details).then(function(user) {
-            if (user){
-                console.log("User signed up, going to main view");
-                $rootScope.my_user = user;
-                $state.go("main.home");
-            }
-        });
+        $scope.user_name_err = false;
+        var abort = false;
+        if (!$scope.username){
+            $scope.user_name_err = true;
+            abort = true;
+        }
+        $scope.first_name_err = false;
+        if (!$scope.first_name){
+            $scope.first_name_err = true;
+            abort = true;
+        }
+        $scope.last_name_err = false;
+        if (!$scope.last_name){
+            $scope.last_name_err = true;
+            abort = true;
+        }
+        $scope.addr_err = false;
+        if (!autocomplete.getPlace()){
+            $scope.addr_err = true;
+            abort = true;
+        }
+        if (!abort){
+            var address = autocomplete.getPlace();
+            address.lat = address.geometry.location.lat();
+            address.lng = address.geometry.location.lng();
+            var user_details = {
+                "user_name"  : $scope.username,
+                "first_name" : $scope.first_name,
+                "last_name"  : $scope.last_name,
+                "address"    : address
+            };
+            console.log("HOLA! ", user_details);
+            LoginService.user_signup(user_details).then(function(user) {
+                if (user){
+                    console.log("User signed up, going to main view");
+                    $rootScope.my_user = user;
+                    $state.go("main.home");
+                }
+            });
+        }
     }
 }]);
