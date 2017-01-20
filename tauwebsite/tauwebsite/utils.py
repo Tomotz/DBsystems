08cb643b85813d4403dfa82dc18473f82bb3874c
Tomotz <tom.mottes@gmail.com>
2017-01-20 11:11:43 +0200

    added 2 places for top notch

--- tauwebsite/tauwebsite/utils.py
+++ tauwebsite/tauwebsite/utils.py
@@ -392,9 +392,10 @@ class DBUtils:
         for place_type in ["Restaurant", "Bar", "Club", "Hotel", "Shop"]:
             placesInDistQuery_orderedByDist = placesInDistQuery + "\nORDER BY rating desc"
             cursor.execute(placesInDistQuery_orderedByDist, (my_lat, my_lat, my_lon, place_type, 3))
-            topCatagory = cursor.fetchone()
-            if topCatagory != None:
-                places.append(topCatagory)
+            for i in range(2):
+                topCatagory = cursor.fetchone()
+                if topCatagory != None:
+                    places.append(topCatagory)
         return places
 
 
